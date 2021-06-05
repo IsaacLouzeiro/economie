e6ccee4d990d4eea6bdec6d5ce29a36cc5cec160
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-06-05 14:19:10 -0300

    arrumando o radio button para a mudanca de cor

--- js/cor-modo.js
+++ js/cor-modo.js
@@ -1,11 +1,17 @@
 // pegamos o valor no localStorage
 const nightModeStorage = localStorage.getItem('gmtNightMode')
 const nightMode = document.querySelector('#night-mode')
+const lightModeStorage = localStorage.getItem('gmtlightMode')
+const lightMode = document.querySelector('#light-mode')
 
 // caso tenha o valor no localStorage
 if (nightModeStorage) {
   // ativa o night mode
   document.documentElement.classList.add('night-mode')
+
+  // já deixa o input marcado como ativo
+  lightMode.checked = false
+
 
   // já deixa o input marcado como ativo
   nightMode.checked = true
@@ -14,7 +20,7 @@ if (nightModeStorage) {
 // ao clicar mudaremos as cores
 nightMode.addEventListener('click', () => {
   // adiciona a classe `night-mode` ao html
-  document.documentElement.classList.toggle('night-mode')
+  document.documentElement.classList.add('night-mode')
 
   // se tiver a classe night-mode
   if (document.documentElement.classList.contains('night-mode')) {
@@ -24,4 +30,23 @@ nightMode.addEventListener('click', () => {
   }
   // senão remove
   localStorage.removeItem('gmtNightMode')
+})
+
+
+
+// ao clicar mudaremos as cores
+lightMode.addEventListener('click', () => {
+  // remove a classe `night-mode` do html
+  document.documentElement.classList.remove('night-mode')
+
+  // se não tiver a classe night-mode
+  if ( ! document.documentElement.classList.contains('night-mode')) {
+    // remove o tema escuro no localStorage
+    localStorage.removeItem('gmtNightMode', true)
+    // salva o tema claro no localStorage
+    localStorage.setItem('gmtlightMode', true)
+    return
+  }
+  // senão remove
+  localStorage.removeItem('gmtlightMode')
 })
