ef8aae0e4013df3ce4bef5f6900436400b582344
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-06-28 18:06:55 -0300

    inserindo o quiz de cheque especial

---
+++ js/jsChequeEspecial/jsquiz.js
@@ -0,0 +1,420 @@
+var questions = [{
+	tamanhoPorcento: "10%",
+	numQuestao: "1/10",
+	numQuestao2: "Questão 1",
+	question: "O que é o Cheque Especial?",
+	choices: ["O Cheque Especial é um crédito que o banco te empresta.", "O cheque é um título de crédito", "O cheque é uma ordem de pagamento à vista"],
+	correctAnswer: 0
+}, {
+	tamanhoPorcento: "20%",
+	numQuestao: "2/10",
+	numQuestao2: "Questão 2",
+	question: "Como funciona o cheque especial?",
+	choices: ["Um credito é liberado na sua conta todos os meses", "O crédito fica disponível na conta corrente para utilização em situações emergenciais.", "Consiste em uma ordem de pagamento à vista emitida em favor de terceiro."],
+	correctAnswer: 1
+}, {
+	tamanhoPorcento: "30%",
+	numQuestao: "3/10",
+	numQuestao2: "Questão 3",
+	question: "Qual a taxa de juros do cheque especial?",
+	choices: ["3% ao mês", "10% ao mês", "8% ao mês"],
+	correctAnswer: 2
+}, {
+	tamanhoPorcento: "40%",
+	numQuestao: "4/10",
+	numQuestao2: "Questão 4",
+	question: "Quais os beneficios de pegar cheque especial",
+	choices: ["Você tem dinheiro na hora,você pode usar pra pagar qualquer divida ", "dinheiro cai na hora,você so pode usar dinheiro pra comprar coisas em lugares especifico", "tem todas as vantagens o dinheiro pode ser usado para qualquer coisa","nenhuma vantagem porque não é algo bom"],
+	correctAnswer: 3
+}, {
+	tamanhoPorcento: "50%",
+	numQuestao: "5/10",
+	numQuestao2: "Questão 5",
+	question: "Por que não devo usar cheque especial?",
+	choices: ["os juros são desproporcionais e abusivo,", "você deve usar sim é um dinheiro que banco te da para emergências"],
+	correctAnswer: 0
+}, {
+	tamanhoPorcento: "60%",
+	numQuestao: "6/10",
+	numQuestao2: "Questão 6",
+	question: "Falando agora falando em hipóteses, uma instuição financeira pode me impor um limite minimo de 500 reais ou mais em caso de eu pegar cheque especial ?",
+	choices: ["sim pode impor um limite minomo mas baseado na sua renda ", "Não pode impor um limite minimo", "sim o banco pode porém o limite minimo é 300 reais "],
+	correctAnswer: 1
+
+}, {
+	tamanhoPorcento: "70%",
+	numQuestao: "7/10",
+	numQuestao2: "Questão 7",
+	question: "Por que o cheque especial tem juros tão altos?",
+	choices: ["Os benefícios do cheque entram no valor dos juros.", "o  valor da taxa é baseado no seu lucro mensal","O banco pré-aprova sem garantia que você vai pagar"],
+	correctAnswer: 2
+}, {
+	tamanhoPorcento: "80%",
+	numQuestao: "8/10",
+	numQuestao2: "Questão 8",
+	question: "O que acontece se eu não tiver dinheiro na conta na data progrmada pra cobrar o cheque especial?",
+	choices: ["ele acumula o juros e assim que o saldo estiver no valor é descontado ","valor acumula pro mês seguinte",],
+	correctAnswer: 0
+}, {
+	tamanhoPorcento: "90%",
+	numQuestao: "9/10",
+	numQuestao2: "Questão 9",
+	question: "A instituição pode alterar meu limite do cheque especial?",
+	choices: ["Não pode devido o cotrato não ser permitdo alteração ", "sim pode com autorização do cliente,sem autorização deve ser feito aiso até 30 dias  "],
+	correctAnswer: 1
+
+}, {
+	tamanhoPorcento: "100%",
+	numQuestao: "10/10",
+	numQuestao2: "Questão 10",
+	question: "Posso fazer cancelamento do meu limite de cheque especial a qualquer momento?",
+	choices: ["sim posso", "não, tenho que esperar cerca de 3 meses "],
+	correctAnswer: 0
+
+}];
+
+var porcentagemQuestao = 0;
+
+var numeroQuestao = 0;
+var numeroQuestao2 = 0;
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+
+$(document).ready(function () {
+
+	// Display the first question
+	displayCurrentQuestion();
+	$(this).find(".quizMessage").hide();
+
+	// On clicking next, display the next question
+	$(this).find(".nextButton").on("click", function () {
+		if (!quizOver) {
+
+			value = $("input[type='radio']:checked").val();
+
+			if (value == undefined) {
+				$(document).find(".quizMessage").text("Por favor, selecione uma resposta!");
+				$(document).find(".quizMessage").show();
+			} else {
+				// TODO: Remove any message -> not sure if this is efficient to call this each time....
+				$(document).find(".quizMessage").hide();
+
+				if (value == questions[currentQuestion].correctAnswer) {
+					correctAnswers++;
+				}
+
+				porcentagemQuestao++;
+				currentQuestion++; // Since we have already displayed the first question on DOM ready
+				numeroQuestao++;
+				numeroQuestao2++;
+
+				if (currentQuestion < questions.length) {
+					displayCurrentQuestion();
+				} else {
+					displayScore();
+					//                    $(document).find(".nextButton").toggle();
+					//                    $(document).find(".playAgainButton").toggle();
+					// Change the text in the next button to ask if user wants to play again
+					$(document).find(".nextButton").text("Repetir");
+					quizOver = true;
+				}
+			}
+		} else { // quiz is over and clicked the next button (which now displays 'Play Again?'
+			quizOver = false;
+			$(document).find(".nextButton").text("Próximo");
+			resetQuiz();
+			displayCurrentQuestion();
+			hideScore();
+		}
+	});
+
+	if (document.createElement('svg').getAttributeNS) {
+
+		function createSVGEl(def) {
+			var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+			if (def) {
+				svg.setAttributeNS(null, 'viewBox', def.viewBox);
+				svg.setAttributeNS(null, 'preserveAspectRatio', def.preserveAspectRatio);
+			} else {
+				svg.setAttributeNS(null, 'viewBox', '0 0 100 100');
+			}
+			svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+			return svg;
+		}
+
+		function controlCheckbox(el, type, svgDef) {
+			var svg = createSVGEl(svgDef);
+			el.parentNode.appendChild(svg);
+
+			el.addEventListener('change', function () {
+				if (el.checked) {
+					draw(el, type);
+				} else {
+					reset(el);
+				}
+			});
+		}
+
+		function controlRadiobox(el, type) {
+			var svg = createSVGEl();
+			el.parentNode.appendChild(svg);
+			el.addEventListener('change', function () {
+				resetRadio(el);
+				draw(el, type);
+			});
+		}
+
+		checkbxsCross.forEach(function (el, i) {
+			controlCheckbox(el, 'cross');
+		});
+		radiobxsFill.forEach(function (el, i) {
+			controlRadiobox(el, 'fill');
+		});
+		checkbxsCheckmark.forEach(function (el, i) {
+			controlCheckbox(el, 'checkmark');
+		});
+		radiobxsCircle.forEach(function (el, i) {
+			controlRadiobox(el, 'circle');
+		});
+		checkbxsBoxfill.forEach(function (el, i) {
+			controlCheckbox(el, 'boxfill');
+		});
+		radiobxsSwirl.forEach(function (el, i) {
+			controlRadiobox(el, 'swirl');
+		});
+		checkbxsDiagonal.forEach(function (el, i) {
+			controlCheckbox(el, 'diagonal');
+		});
+		checkbxsList.forEach(function (el) {
+			controlCheckbox(el, 'list', {
+				viewBox: '0 0 300 100',
+				preserveAspectRatio: 'none'
+			});
+		});
+
+		function draw(el, type) {
+			var paths = [],
+				pathDef,
+				animDef,
+				svg = el.parentNode.querySelector('svg');
+
+			switch (type) {
+				case 'cross':
+					pathDef = pathDefs.cross;
+					animDef = animDefs.cross;
+					break;
+				case 'fill':
+					pathDef = pathDefs.fill;
+					animDef = animDefs.fill;
+					break;
+				case 'checkmark':
+					pathDef = pathDefs.checkmark;
+					animDef = animDefs.checkmark;
+					break;
+				case 'circle':
+					pathDef = pathDefs.circle;
+					animDef = animDefs.circle;
+					break;
+				case 'boxfill':
+					pathDef = pathDefs.boxfill;
+					animDef = animDefs.boxfill;
+					break;
+				case 'swirl':
+					pathDef = pathDefs.swirl;
+					animDef = animDefs.swirl;
+					break;
+				case 'diagonal':
+					pathDef = pathDefs.diagonal;
+					animDef = animDefs.diagonal;
+					break;
+				case 'list':
+					pathDef = pathDefs.list;
+					animDef = animDefs.list;
+					break;
+			};
+
+			paths.push(document.createElementNS('http://www.w3.org/2000/svg', 'path'));
+
+			if (type === 'cross' || type === 'list') {
+				paths.push(document.createElementNS('http://www.w3.org/2000/svg', 'path'));
+			}
+
+			for (var i = 0, len = paths.length; i < len; ++i) {
+				var path = paths[i];
+				svg.appendChild(path);
+
+				path.setAttributeNS(null, 'd', pathDef[i]);
+
+				var length = path.getTotalLength();
+				// Clear any previous transition
+				//path.style.transition = path.style.WebkitTransition = path.style.MozTransition = 'none';
+				// Set up the starting positions
+				path.style.strokeDasharray = length + ' ' + length;
+				if (i === 0) {
+					path.style.strokeDashoffset = Math.floor(length) - 1;
+				} else path.style.strokeDashoffset = length;
+				// Trigger a layout so styles are calculated & the browser
+				// picks up the starting position before animating
+				path.getBoundingClientRect();
+				// Define our transition
+				path.style.transition = path.style.WebkitTransition = path.style.MozTransition = 'stroke-dashoffset ' + animDef.speed + 's ' + animDef.easing + ' ' + i * animDef.speed + 's';
+				// Go!
+				path.style.strokeDashoffset = '0';
+			}
+		}
+
+		function reset(el) {
+			Array.prototype.slice.call(el.parentNode.querySelectorAll('svg > path')).forEach(function (el) {
+				el.parentNode.removeChild(el);
+			});
+		}
+
+		function resetRadio(el) {
+			Array.prototype.slice.call(document.querySelectorAll('input[type="radio"][name="' + el.getAttribute('name') + '"]')).forEach(function (el) {
+				var path = el.parentNode.querySelector('svg > path');
+				if (path) {
+					path.parentNode.removeChild(path);
+				}
+			});
+		}
+
+	}
+
+});
+
+// This displays the current question AND the choices
+function displayCurrentQuestion() {
+
+	console.log("In display current Question");
+	
+	var tamanhoPorcento = questions[porcentagemQuestao].tamanhoPorcento;
+	var tamanhoPorcentoClass = $(document).find(".quizContainer > #porcentagem");
+
+	var numQuestao = questions[numeroQuestao].numQuestao;
+	var numQuestaoClass = $(document).find(".quizContainer > .numQuestion");
+	var numQuestao2 = questions[numeroQuestao2].numQuestao2;
+	var numQuestaoClass2 = $(document).find(".quizContainer > .numQuestion2");
+	var question = questions[currentQuestion].question;
+	var questionClass = $(document).find(".quizContainer > .question");
+	var choiceList = $(document).find(".quizContainer > .choiceList");
+	var numChoices = questions[currentQuestion].choices.length;
+
+
+	$(tamanhoPorcentoClass).text(tamanhoPorcento);
+	
+	switch (porcentagemQuestao) {
+		case 0:
+			
+			document.getElementById("porcentagem").style.width = "5%";
+
+			break;
+		
+		case 1:
+			
+			 document.getElementById("porcentagem").style.width = "15%";
+			break;
+		
+		case 2:
+			
+			 document.getElementById("porcentagem").style.width = "25%";
+
+			break;
+
+		case 3:
+			
+			 document.getElementById("porcentagem").style.width = "35%";
+
+			break;
+
+		case 4:
+			
+			 document.getElementById("porcentagem").style.width = "45%";
+
+			break;
+
+		case 5:
+			
+			 document.getElementById("porcentagem").style.width = "55%";
+
+			break;
+		
+		case 6:
+			
+			 document.getElementById("porcentagem").style.width = "65%";
+
+			break;
+
+		case 7:
+			
+			 document.getElementById("porcentagem").style.width = "75%";
+
+			break;
+
+		case 8:
+			
+			 document.getElementById("porcentagem").style.width = "85%";
+
+			break;
+
+		case 9:
+			
+			 document.getElementById("porcentagem").style.width = "100%";
+
+			break;
+	
+		default:
+			break;
+	};
+
+
+	// Set the questionClass text to the current question
+	$(numQuestaoClass).text(numQuestao);
+
+	// Set the questionClass text to the current question
+	$(numQuestaoClass2).text(numQuestao2);
+
+	// Set the questionClass text to the current question
+	$(questionClass).text(question);
+
+
+	// Remove all current <li> elements (if any)
+	$(choiceList).find("li").remove();
+
+	var choice;
+	for (i = 0; i < numChoices; i++) {
+		choice = questions[currentQuestion].choices[i];
+		$('<li><input type="radio" value=' + i + ' id="dynradio' + i + '" name="dynradio" /><label for="dynradio' + i + '">' + choice + '</label></li>').appendTo(choiceList);
+	}
+}
+
+function resetQuiz() {
+	porcentagemQuestao = 0;
+	numeroQuestao = 0;
+	numeroQuestao2 = 0;
+	currentQuestion = 0;
+	correctAnswers = 0;
+	$(document).find(".quizContainer > .numQuestion").show();
+	$(document).find(".quizContainer > .numQuestion2").show();
+	$(document).find(".question").show();
+	$(document).find(".question").show();
+	$(document).find(".choiceList").show();
+	$(document).find('.fim').hide();
+	hideScore();
+	
+}
+
+function displayScore() {
+	$(document).find(".quizContainer > .numQuestion").hide();
+	$(document).find(".quizContainer > .numQuestion2").hide();
+	$(document).find(".question").hide();
+	$(document).find(".quizContainer > .result").text("Você acertou " + correctAnswers + " de " + questions.length);
+	$(document).find(".quizContainer > .result").show();
+	$(document).find('.fim').show();
+	$(document).find('.fim').text("Fim");
+	$(document).find(".choiceList").hide();
+}
+
+function hideScore() {
+	$(document).find(".result").hide();
+}
